85b194b70ecaeff20727a1a32130934593a50c67
Genesis <84639113+gescobar19@users.noreply.github.com>
2026-06-25 04:18:11 -0400

    updated backend test

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -4,7 +4,7 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from backend.app.main import app
+from app.main import app
 
 client = TestClient(app)
 
