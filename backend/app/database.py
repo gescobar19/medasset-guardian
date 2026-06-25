57db302eb0fdf31bbcab4d28db07445f52c05e98
Genesis <84639113+gescobar19@users.noreply.github.com>
2026-06-25 04:30:31 -0400

    updated workflow ci and database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,19 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
-# Load environment variables from .env
 load_dotenv()
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "postgresql+psycopg2://summitsafe:summitsafe_pw@localhost:5432/summitsafe"
 )
-print("DATABASE_URL =", DATABASE_URL)
+
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True  # helps prevent stale connections
+    pool_pre_ping=True
 )
-print("ENGINE =", engine.url)
 
 SessionLocal = sessionmaker(
     autocommit=False,
